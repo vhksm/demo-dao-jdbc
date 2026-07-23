a8d665266bc00bda0f699901c11b4274689c8353
vhksm <victorsimaodemiranda@outlook.com>
2026-07-23 15:59:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDateTime;
 
 public class Program {
     public static void main(String[] args) {
+
         Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(24, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.00, obj);
+
         System.out.println(obj);
+
+        System.out.println(seller);
     }
 }
