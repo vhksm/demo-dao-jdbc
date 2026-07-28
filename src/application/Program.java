6861938d4b996bbb2a36b933b2bfac40304534fe
vhksm <victorsimaodemiranda@outlook.com>
2026-07-28 15:52:24 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,5 +38,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDateTime.of(1988, 02, 22, 0, 0, 0), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New ID = " + newSeller.getId());
+
+        System.out.println("\n === TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
     }
 }
